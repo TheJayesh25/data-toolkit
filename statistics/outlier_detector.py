9dd35831e02da1ffe6071176ee29b086395884f2
Jayesh Suryawanshi <jps25102001@gmail.com>
2025-10-02 10:12:00 +0530

    add outlier detector utility

---
+++ statistics/outlier_detector.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def detect_outliers(df: pd.DataFrame, column):
+
+    q1 = df[column].quantile(0.25)
+    q3 = df[column].quantile(0.75)
+
+    iqr = q3 - q1
+
+    lower = q1 - 1.5 * iqr
+    upper = q3 + 1.5 * iqr
+
+    return df[(df[column] < lower) | (df[column] > upper)]

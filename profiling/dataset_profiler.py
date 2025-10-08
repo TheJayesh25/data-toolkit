6eb469db0ff4df507a4803fd814edd004489f898
Jayesh Suryawanshi <jps25102001@gmail.com>
2025-10-08 09:24:00 +0530

    adds missing counts in dataset profiler

--- profiling/dataset_profiler.py
+++ profiling/dataset_profiler.py
@@ -2,8 +2,11 @@ import pandas as pd
 
 def profile_dataset(df: pd.DataFrame):
 
-    return {
+    profile = {
         "rows": df.shape[0],
         "columns": df.shape[1],
-        "dtypes": df.dtypes.astype(str).to_dict()
+        "dtypes": df.dtypes.astype(str).to_dict(),
+        "missing_values": df.isna().sum().to_dict()
     }
+
+    return profile

785e06eb1228b5433bf152d3873670b1d67d7482
Jayesh Suryawanshi <jps25102001@gmail.com>
2025-10-08 09:13:00 +0530

    adds percentage in missing value analyzer

--- cleaning/missing_value_analyzer.py
+++ cleaning/missing_value_analyzer.py
@@ -3,7 +3,9 @@ import pandas as pd
 def missing_summary(df: pd.DataFrame):
 
     missing = df.isna().sum()
+    percent = (missing / len(df)) * 100
 
     return pd.DataFrame({
-        "missing_count": missing
-    })
+        "missing_count": missing,
+        "missing_percent": percent
+    }).sort_values("missing_percent", ascending=False)

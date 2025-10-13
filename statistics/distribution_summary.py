8d96e2af226e3b07ebf3336b4e78e34270abe620
Jayesh Suryawanshi <jps25102001@gmail.com>
2025-10-13 08:18:00 +0530

    adds min max in distribution summary

--- statistics/distribution_summary.py
+++ statistics/distribution_summary.py
@@ -5,5 +5,7 @@ def distribution_summary(df: pd.DataFrame, column):
     return {
         "mean": df[column].mean(),
         "median": df[column].median(),
-        "std": df[column].std()
+        "std": df[column].std(),
+        "min": df[column].min(),
+        "max": df[column].max()
     }

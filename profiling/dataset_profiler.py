cb2f6b6e89b534a6b6af015d3fee6db08caa8132
Jayesh Suryawanshi <jps25102001@gmail.com>
2025-10-18 08:36:00 +0530

    adds unique counts in dataset profiler

--- profiling/dataset_profiler.py
+++ profiling/dataset_profiler.py
@@ -6,7 +6,8 @@ def profile_dataset(df: pd.DataFrame):
         "rows": df.shape[0],
         "columns": df.shape[1],
         "dtypes": df.dtypes.astype(str).to_dict(),
-        "missing_values": df.isna().sum().to_dict()
+        "missing_values": df.isna().sum().to_dict(),
+        "unique_values": df.nunique().to_dict()
     }
 
     return profile
